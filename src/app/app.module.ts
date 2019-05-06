19d762ead75086c3d1555ce388ffc9346e9c051a
Mojila <ajilaksono@outlook.co.id>
2019-05-06 09:43:17 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigatorComponent,
+    SubHeaderComponent
   ],
   imports: [
     BrowserModule,
